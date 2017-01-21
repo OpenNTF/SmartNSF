ac9faffeecbb3593c3373e75f8250f3f805b1f82
Christian Guedemann <christian.guedemann@webgate.biz>
2017-01-21 19:05:08 +0100

    Discussion Template extended with rooter.groovy

--- server/org.openntf.xrest.xsp.testsuite/src/org/openntf/xrest/xsp/testsuite/discussion_router.groovy
+++ server/org.openntf.xrest.xsp.testsuite/src/org/openntf/xrest/xsp/testsuite/discussion_router.groovy
@@ -1,61 +1,96 @@
 router.GET('topics}') {
-	strategy(ALL_FROM_VIEW) {
+	strategy(SELECT_ALL_DOCUMENTS_BY_VIEW) {
 		viewName {
 			
 		}
 	}
-	mapJson "company", json:'company',type:'String'
-	mapJson "fdFirstName", json:'firstname', type:'String'
+	mapJson "date", json:'date',type:'STRING',isformula:true, formula:'@Created'
+	mapJson "Subject", json:'topic', type:'STRING'
+	mapJson "author", json:'author', type:'STRING',isformula:true,formula:'@Name([CN]; From)'
 }
-
-
 router.GET('topics/{id}') {
-	strategy(SELECT_DOCUMENT_FROM_VIEW_BY_KEY) {
-		keyVariableName("{id}")
-		viewName("customerById")
-	}
-	accessPermission "SalesManager","[CustomerService]"
-	mapJson "company", json:'company',type:'String'
-	mapJson "fdFirstName", json:'firstname', type:'String'
-	events VALIDATE: {
-		context -> return true}
-}
-
-router.GET('customers/{id}/contract/{attachmentName}') {
-	strategy(SELECT_ATTACHMENT) {
-		documentStrategy(SELECT_DOCUMENT_FROM_VIEW_BY_KEY) {
+	strategy(SELECT_DOCUMENT_BY_UNID) {
 			keyVariableName("{id}")
-			viewName("customerById")
+	}
+	mapJson "date", json:'date',type:'STRING',isformula:true, formula:'@Created'
+	mapJson "Subject", json:'topic', type:'STRING'
+	mapJson "author", json:'author', type:'STRING',isformula:true, formula:'@Name([CN]; From)'
+	mapJson "body", json:'content', type:'MIME'
+	mapJson "categories", json:'categories', type:'ARRY_OF_STRING'
+}
+router.GET('topics/{id}/attachment/{attachmentName}') {
+	strategy(SELECT_ATTACHMENT) {
+		documentStrategy(SELECT_DOCUMENT_BY_UNID) {
+			keyVariableName("{id}")
 		}
 		fieldName "Body"
 		selectionType BY_NAME //Could be BY_NAME, FIRST
 		attachmentNameVariableName "{attachmentName}"
 	}
 }
-
-router.POST('customers/{id}/contract') {
-	strategy(SELECT_ATTACHMENT){
-		documentStrategy(SELECT_DOCUMENT_FROM_VIEW_BY_KEY) {
-			keyVariableName("{id}")
-			viewName("customerById")
-		}
-		fieldName "Body"
-		updateType REPLACE_ALL //Could be REPLACE_ALL, REPLACE_BY_NAME
-	}
-}
-
-router.POST('comment/{id}') {
+router.POST('topics/{id}') {
 	strategy(SELECT_DOCUMENT_BY_UNID) {
 		keyVariableName("{id}")
 	}
-	mapJson "company", json:'company',type:'String'
-	mapJson "fdFirstName", json:'firstname', type:'String'
-}
-router.DELETE('quote/{id}') {
-	strategy(SELECT_DOCUMENT_FROM_VIEW_BY_KEY) {
-		keyVariableName("{id}")
-		viewName("customerById")
+	mapJson "Subject", json:'topic', type:'STRING'
+	mapJson "body", json:'content', type:'MIME'
+	mapJson "categories", json:'categories', type:'ARRY_OF_STRING'
+	events POST_SAVE_DOCUMENT: {
+		context, document ->
+		nsfHelp = context.getNSFHelper()
+		nsfHelp.computeWithForm(document)
 	}
-	mapJson "company", json:'company',type:'String'
-	mapJson "fdFirstName", json:'firstname', type:'String'
+}
+
+router.POST('topics/{id}/attachment') {
+	strategy(SELECT_ATTACHMENT){
+		documentStrategy(SELECT_DOCUMENT_BY_UNID) {
+			keyVariableName("{id}")
+		}
+		fieldName "Body"
+		updateType REPLACE_BY_NAME //Could be REPLACE_ALL, REPLACE_BY_NAME
+	}
+}
+
+router.GET('comments/byparent/{parent_id}') {
+	strategy(SELECT_ALL_DOCUMENTS_FROM_VIEW_BY_KEY) {
+			keyVariableName("{parent_id}")
+			viewName("commentsByParentId")
+	}
+	mapJson "date", json:'date',type:'STRING',isformula:true, formula:'@Created'
+	mapJson "Subject", json:'topic', type:'STRING'
+	mapJson "author", json:'author', type:'STRING',isformula:true,formula:'@Name([CN]; From)'
+}
+
+router.GET('comments/{id}') {
+	strategy(SELECT_DOCUMENT_BY_UNID) {
+			keyVariableName("{id}")
+	}
+	mapJson "date", json:'date',type:'STRING',isformula:true, formula:'@Created'
+	mapJson "Subject", json:'topic', type:'STRING'
+	mapJson "author", json:'author', type:'STRING',isformula:true,formula:'@Name([CN]; From)'
+	mapJson "body", json:'content', type:'MIME'
+	mapJson "categories", json:'categories', type:'ARRY_OF_STRING'
+
+}
+
+router.POST('comments/{id}/parent/{parent_id}') {
+	strategy(SELECT_DOCUMENT_BY_UNID) {
+		keyVariableName("{id}")
+	}
+	mapJson "Subject", json:'topic', type:'STRING'
+	mapJson "body", json:'content', type:'MIME'
+	mapJson "categories", json:'categories', type:'ARRY_OF_STRING'
+	events PRE_SAVE_DOCUMENT:{
+		context, document ->
+		parentId = context.getRouterVariables().get('parent_id')
+		nsfHelp = context.getNSFHelper()
+		nsfHelp.makeDocumentAsChild(parentId, document)
+	}
+}
+
+router.DELETE('document/{id}') {
+	strategy(SELECT_DOCUMENT_BY_UNID) {
+		keyVariableName("{id}")
+	}
 }
