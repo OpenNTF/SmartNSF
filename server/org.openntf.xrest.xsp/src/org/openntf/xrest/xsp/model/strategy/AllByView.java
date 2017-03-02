2ba27763441416ce3dc5945c13ac1d72a68774c4
Martin Jinoch <jinoch@gmail.com>
2017-03-02 20:24:37 +0100

    refactor to use ViewNavigator to iterate over view content

--- server/org.openntf.xrest.xsp/src/org/openntf/xrest/xsp/model/strategy/AllByView.java
+++ server/org.openntf.xrest.xsp/src/org/openntf/xrest/xsp/model/strategy/AllByView.java
@@ -3,6 +3,7 @@ package org.openntf.xrest.xsp.model.strategy;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.openntf.xrest.xsp.exec.Context;
 import org.openntf.xrest.xsp.exec.DatabaseProvider;
 import org.openntf.xrest.xsp.exec.ExecutorException;
 import org.openntf.xrest.xsp.exec.convertor.DocumentList2JsonConverter;
@@ -13,29 +14,34 @@ import org.openntf.xrest.xsp.utils.NotesObjectRecycler;
 
 import com.ibm.commons.util.io.json.JsonJavaArray;
 
-import org.openntf.xrest.xsp.exec.Context;
-
 import lotus.domino.Database;
 import lotus.domino.Document;
 import lotus.domino.NotesException;
 import lotus.domino.View;
+import lotus.domino.ViewEntry;
+import lotus.domino.ViewNavigator;
 
-public class AllByView extends AbstractViewDatabaseStrategy implements StrategyModel<DocumentListDataContainer, JsonJavaArray> {
+public class AllByView extends AbstractViewDatabaseStrategy implements
+		StrategyModel<DocumentListDataContainer, JsonJavaArray> {
 
 	private Database dbAccess;
 	private View viewAccess;
 
 	@Override
-	public DocumentListDataContainer buildDataContainer(Context context) throws ExecutorException {
+	public DocumentListDataContainer buildDataContainer(final Context context) throws ExecutorException {
 		try {
-			dbAccess = DatabaseProvider.INSTANCE.getDatabase(getDatabaseNameValue(context), context.getDatabase(), context.getSession());
+			dbAccess = DatabaseProvider.INSTANCE.getDatabase(getDatabaseNameValue(context), context.getDatabase(),
+					context.getSession());
 			viewAccess = dbAccess.getView(getViewNameValue(context));
 			List<Document> docs = new ArrayList<Document>();
-			Document docNext = viewAccess.getFirstDocument();
-			while (docNext != null) {
-				Document docProcess = docNext;
-				docNext = viewAccess.getNextDocument(docNext);
-				docs.add(docProcess);
+			ViewNavigator vnav = viewAccess.createViewNav();
+			ViewEntry entCurrent = vnav.getFirst();
+			while (entCurrent != null && entCurrent.isValid()) {
+				docs.add(dbAccess.getDocumentByUNID(entCurrent.getUniversalID()));
+				ViewEntry nextEntry = vnav.getNext();
+				// recycle!
+				entCurrent.recycle();
+				entCurrent = nextEntry;
 			}
 			return new DocumentListDataContainer(docs);
 		} catch (Exception ex) {
@@ -49,7 +55,8 @@ public class AllByView extends AbstractViewDatabaseStrategy implements StrategyM
 	}
 
 	@Override
-	public JsonJavaArray buildResponse(Context context, RouteProcessor routeProcessor, DataContainer<?> dc) throws NotesException {
+	public JsonJavaArray buildResponse(final Context context, final RouteProcessor routeProcessor,
+			final DataContainer<?> dc) throws NotesException {
 		DocumentListDataContainer docListDC = (DocumentListDataContainer) dc;
 		DocumentList2JsonConverter d2jc = new DocumentList2JsonConverter(docListDC, routeProcessor, context);
 		return d2jc.buildJsonFromDocument();
