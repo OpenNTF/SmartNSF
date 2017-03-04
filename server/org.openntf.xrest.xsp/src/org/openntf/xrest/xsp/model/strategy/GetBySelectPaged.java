e3fed7d3505a0fa67e1e9807aeac4e933282ba96
Martin Jinoch <jinoch@gmail.com>
2017-03-04 12:46:56 +0100

    implement paged version of GetBySelect

---
+++ server/org.openntf.xrest.xsp/src/org/openntf/xrest/xsp/model/strategy/GetBySelectPaged.java
@@ -0,0 +1,82 @@
+package org.openntf.xrest.xsp.model.strategy;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map.Entry;
+
+import org.openntf.xrest.xsp.dsl.DSLBuilder;
+import org.openntf.xrest.xsp.exec.Context;
+import org.openntf.xrest.xsp.exec.DatabaseProvider;
+import org.openntf.xrest.xsp.exec.ExecutorException;
+import org.openntf.xrest.xsp.exec.convertor.DocumentListPaged2JsonConverter;
+import org.openntf.xrest.xsp.exec.datacontainer.DocumentListDataContainer;
+import org.openntf.xrest.xsp.exec.datacontainer.DocumentListPaginationDataContainer;
+import org.openntf.xrest.xsp.model.DataContainer;
+import org.openntf.xrest.xsp.model.RouteProcessor;
+
+import com.ibm.commons.util.io.json.JsonObject;
+
+import groovy.lang.Closure;
+import lotus.domino.Database;
+import lotus.domino.Document;
+import lotus.domino.DocumentCollection;
+import lotus.domino.NotesException;
+
+public class GetBySelectPaged extends AbstractDatabaseStrategy implements StrategyModel<DocumentListDataContainer, JsonObject> {
+
+	private String selectQueryValue;
+	private Closure<?> selectQueryCl;
+
+	public void selectQuery(final String name) {
+		this.selectQueryValue = name;
+	}
+
+	public void selectQuery(final Closure<?> queryCl) {
+		this.selectQueryCl = queryCl;
+	}
+
+	public String getSelectQueryValue(final Context context) {
+		if (selectQueryCl != null) {
+			return (String) DSLBuilder.callClosure(selectQueryCl, context);
+		} else {
+			return selectQueryValue;
+		}
+	}
+
+	@Override
+	public DocumentListDataContainer buildDataContainer(final Context context) throws ExecutorException {
+		try {
+			Database dbAccess = DatabaseProvider.INSTANCE.getDatabase(getDatabaseNameValue(context), context.getDatabase(), context
+					.getSession());
+			List<Document> docs = new ArrayList<Document>();
+			String search = buildSelectString(context);
+			DocumentCollection dcl = dbAccess.search(search);
+			Document docNext = dcl.getFirstDocument();
+			while (docNext != null) {
+				Document docProcess = docNext;
+				docNext = dcl.getNextDocument();
+				docs.add(docProcess);
+			}
+			return new DocumentListDataContainer(docs, null, dbAccess);
+		} catch (Exception ex) {
+			throw new ExecutorException(500, ex, "", "getmodel");
+		}
+
+	}
+
+	private String buildSelectString(final Context context) {
+		String rc = getSelectQueryValue(context);
+		for (Entry<String, String> routeEntry : context.getRouterVariables().entrySet()) {
+			rc = rc.replace("{" + routeEntry.getKey() + "}", routeEntry.getValue());
+		}
+		return rc;
+	}
+
+	@Override
+	public JsonObject buildResponse(final Context context, final RouteProcessor routeProcessor, final DataContainer<?> dc)
+			throws NotesException {
+		DocumentListPaginationDataContainer docListDC = (DocumentListPaginationDataContainer) dc;
+		DocumentListPaged2JsonConverter d2jc = new DocumentListPaged2JsonConverter(docListDC, routeProcessor, context);
+		return d2jc.buildJsonFromDocument();
+	}
+}
