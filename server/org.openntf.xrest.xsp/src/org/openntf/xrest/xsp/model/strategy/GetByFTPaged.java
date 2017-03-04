f9862fb3fd26dc11d3fbdf1aaf2b4eb470b78df3
Martin Jinoch <jinoch@gmail.com>
2017-03-04 12:33:04 +0100

    implement paged version of GetByFT

---
+++ server/org.openntf.xrest.xsp/src/org/openntf/xrest/xsp/model/strategy/GetByFTPaged.java
@@ -0,0 +1,90 @@
+package org.openntf.xrest.xsp.model.strategy;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map.Entry;
+
+import org.openntf.xrest.xsp.dsl.DSLBuilder;
+import org.openntf.xrest.xsp.exec.Context;
+import org.openntf.xrest.xsp.exec.DatabaseProvider;
+import org.openntf.xrest.xsp.exec.ExecutorException;
+import org.openntf.xrest.xsp.exec.convertor.DocumentList2JsonConverter;
+import org.openntf.xrest.xsp.exec.datacontainer.DocumentListDataContainer;
+import org.openntf.xrest.xsp.exec.datacontainer.DocumentListPaginationDataContainer;
+import org.openntf.xrest.xsp.model.DataContainer;
+import org.openntf.xrest.xsp.model.RouteProcessor;
+
+import com.ibm.commons.util.io.json.JsonJavaArray;
+
+import groovy.lang.Closure;
+import lotus.domino.Database;
+import lotus.domino.Document;
+import lotus.domino.DocumentCollection;
+import lotus.domino.NotesException;
+
+public class GetByFTPaged extends AbstractDatabaseStrategy implements StrategyModel<DocumentListPaginationDataContainer, JsonJavaArray> {
+
+	private String ftQueryValue;
+	private Closure<?> ftQueryValueCl;
+
+	public String getFtQueryValue(final Context context) {
+		if (ftQueryValueCl != null) {
+			return (String) DSLBuilder.callClosure(ftQueryValueCl, context);
+		} else {
+			return ftQueryValue;
+		}
+	}
+
+	public void ftQueryValue(final String keyVariableValue) {
+		this.ftQueryValue = keyVariableValue;
+	}
+
+	public void ftQueryValue(final Closure<?> keyVariableCl) {
+		this.ftQueryValueCl = keyVariableCl;
+	}
+
+	@Override
+	public DocumentListPaginationDataContainer buildDataContainer(final Context context) throws ExecutorException {
+		try {
+			Database dbAccess = DatabaseProvider.INSTANCE.getDatabase(getDatabaseNameValue(context), context.getDatabase(), context
+					.getSession());
+			int start = getParamIntValue(context.getRequest().getParameter("start"), DEFAULT_START);
+			int count = getParamIntValue(context.getRequest().getParameter("count"), DEFAULT_COUNT);
+			String search = buildSearchString(context);
+			DocumentCollection dcl = dbAccess.FTSearch(search);
+			Document docNext = null;
+			if (start > 1) {
+				docNext = dcl.getNthDocument(start);
+			} else {
+				docNext = dcl.getFirstDocument();
+			}
+			List<Document> docs = new ArrayList<Document>();
+			int i = 0;
+			while (docNext != null && i < count) {
+				i++;
+				Document docProcess = docNext;
+				docNext = dcl.getNextDocument();
+				docs.add(docProcess);
+			}
+			return new DocumentListPaginationDataContainer(docs, start, dcl.getCount(), null, dbAccess);
+		} catch (Exception ex) {
+			throw new ExecutorException(500, ex, "", "getmodel");
+		}
+	}
+
+	private String buildSearchString(final Context context) {
+		String rc = getFtQueryValue(context);
+		for (Entry<String, String> routeEntry : context.getRouterVariables().entrySet()) {
+			rc = rc.replace("{" + routeEntry.getKey() + "}", routeEntry.getValue());
+		}
+		return rc;
+	}
+
+	@Override
+	public JsonJavaArray buildResponse(final Context context, final RouteProcessor routeProcessor, final DataContainer<?> dc)
+			throws NotesException {
+		DocumentListDataContainer docListDC = (DocumentListDataContainer) dc;
+		DocumentList2JsonConverter d2jc = new DocumentList2JsonConverter(docListDC, routeProcessor, context);
+		return d2jc.buildJsonFromDocument();
+	}
+}
