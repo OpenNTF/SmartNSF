55d51d6c4510091ffcc6006a75ea0f9ee5e0737d
Martin Jinoch <jinoch@gmail.com>
2017-03-04 13:00:50 +0100

    ehm, now add the paging for real

--- server/org.openntf.xrest.xsp/src/org/openntf/xrest/xsp/model/strategy/GetBySelectPaged.java
+++ server/org.openntf.xrest.xsp/src/org/openntf/xrest/xsp/model/strategy/GetBySelectPaged.java
@@ -9,7 +9,6 @@ import org.openntf.xrest.xsp.exec.Context;
 import org.openntf.xrest.xsp.exec.DatabaseProvider;
 import org.openntf.xrest.xsp.exec.ExecutorException;
 import org.openntf.xrest.xsp.exec.convertor.DocumentListPaged2JsonConverter;
-import org.openntf.xrest.xsp.exec.datacontainer.DocumentListDataContainer;
 import org.openntf.xrest.xsp.exec.datacontainer.DocumentListPaginationDataContainer;
 import org.openntf.xrest.xsp.model.DataContainer;
 import org.openntf.xrest.xsp.model.RouteProcessor;
@@ -22,7 +21,7 @@ import lotus.domino.Document;
 import lotus.domino.DocumentCollection;
 import lotus.domino.NotesException;
 
-public class GetBySelectPaged extends AbstractDatabaseStrategy implements StrategyModel<DocumentListDataContainer, JsonObject> {
+public class GetBySelectPaged extends AbstractDatabaseStrategy implements StrategyModel<DocumentListPaginationDataContainer, JsonObject> {
 
 	private String selectQueryValue;
 	private Closure<?> selectQueryCl;
@@ -44,20 +43,29 @@ public class GetBySelectPaged extends AbstractDatabaseStrategy implements Strate
 	}
 
 	@Override
-	public DocumentListDataContainer buildDataContainer(final Context context) throws ExecutorException {
+	public DocumentListPaginationDataContainer buildDataContainer(final Context context) throws ExecutorException {
 		try {
 			Database dbAccess = DatabaseProvider.INSTANCE.getDatabase(getDatabaseNameValue(context), context.getDatabase(), context
 					.getSession());
-			List<Document> docs = new ArrayList<Document>();
 			String search = buildSelectString(context);
 			DocumentCollection dcl = dbAccess.search(search);
-			Document docNext = dcl.getFirstDocument();
-			while (docNext != null) {
+			int start = getParamIntValue(context.getRequest().getParameter("start"), DEFAULT_START);
+			int count = getParamIntValue(context.getRequest().getParameter("count"), DEFAULT_COUNT);
+			Document docNext = null;
+			if (start > 1) {
+				docNext = dcl.getNthDocument(start);
+			} else {
+				docNext = dcl.getFirstDocument();
+			}
+			List<Document> docs = new ArrayList<Document>();
+			int i = 0;
+			while (docNext != null && i < count) {
+				i++;
 				Document docProcess = docNext;
 				docNext = dcl.getNextDocument();
 				docs.add(docProcess);
 			}
-			return new DocumentListDataContainer(docs, null, dbAccess);
+			return new DocumentListPaginationDataContainer(docs, start, dcl.getCount(), null, dbAccess);
 		} catch (Exception ex) {
 			throw new ExecutorException(500, ex, "", "getmodel");
 		}
