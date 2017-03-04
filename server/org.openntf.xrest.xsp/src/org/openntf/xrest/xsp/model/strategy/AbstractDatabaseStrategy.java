3f4234ce89111e811d22d3e9818dab6b9c72dfb2
Martin Jinoch <jinoch@gmail.com>
2017-03-04 12:32:45 +0100

    refactor: pull up method and fields to allow reuse in GetByFTPaged

--- server/org.openntf.xrest.xsp/src/org/openntf/xrest/xsp/model/strategy/AbstractDatabaseStrategy.java
+++ server/org.openntf.xrest.xsp/src/org/openntf/xrest/xsp/model/strategy/AbstractDatabaseStrategy.java
@@ -7,6 +7,8 @@ import groovy.lang.Closure;
 
 public class AbstractDatabaseStrategy {
 
+	protected static final int DEFAULT_START = 1;
+	protected static final int DEFAULT_COUNT = 10;
 	private String databaseNameValue;
 	private Closure<?> databaseNameCl;
 
@@ -29,6 +31,28 @@ public class AbstractDatabaseStrategy {
 		} else {
 			return databaseNameValue;
 		}
+	}
+
+	/**
+	 * Converts given param to int, assings defaultVal when it is empty or less
+	 * than 1
+	 * 
+	 * @param param
+	 * @param defaultVal
+	 * @return int value of param or defaulVal
+	 */
+	protected int getParamIntValue(final String param, final int defaultVal) {
+		int ret = defaultVal;
+		if (null != param && !param.isEmpty()) {
+			try {
+				ret = Integer.parseInt(param);
+				if (ret < 1) {
+					ret = defaultVal;
+				}
+			} catch (NumberFormatException e) {
+			}
+		}
+		return ret;
 	}
 
 }
--- server/org.openntf.xrest.xsp/src/org/openntf/xrest/xsp/model/strategy/AbstractViewDatabaseStrategy.java
+++ server/org.openntf.xrest.xsp/src/org/openntf/xrest/xsp/model/strategy/AbstractViewDatabaseStrategy.java
@@ -7,8 +7,6 @@ import groovy.lang.Closure;
 
 public class AbstractViewDatabaseStrategy extends AbstractDatabaseStrategy {
 
-	protected static final int DEFAULT_START = 1;
-	protected static final int DEFAULT_COUNT = 10;
 	private String viewNameValue;
 	private Closure<?> viewNameCl;
 
@@ -30,28 +28,6 @@ public class AbstractViewDatabaseStrategy extends AbstractDatabaseStrategy {
 		} else {
 			return viewNameValue;
 		}
-	}
-
-	/**
-	 * Converts given param to int, assings defaultVal when it is empty or less
-	 * than 1
-	 * 
-	 * @param param
-	 * @param defaultVal
-	 * @return int value of param or defaulVal
-	 */
-	protected int getParamIntValue(final String param, final int defaultVal) {
-		int ret = defaultVal;
-		if (null != param && !param.isEmpty()) {
-			try {
-				ret = Integer.parseInt(param);
-				if (ret < 1) {
-					ret = defaultVal;
-				}
-			} catch (NumberFormatException e) {
-			}
-		}
-		return ret;
 	}
 
 }
