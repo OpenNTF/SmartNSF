b19c924298004ef0f977e6b5851a2e4623b790a6
Martin Jinoch <jinoch@gmail.com>
2017-03-06 22:00:49 +0100

    allow specifying key matching mode
    implement mode("exact") or mode("partial") (default, can be omitted) for
    SELECT_ALL_DOCUMENTS_FROM_VIEW_BY_KEY and
    SELECT_ALL_DOCUMENTS_FROM_VIEW_BY_KEY_PAGED strategies

--- server/org.openntf.xrest.xsp/src/org/openntf/xrest/xsp/model/strategy/AllByKey.java
+++ server/org.openntf.xrest.xsp/src/org/openntf/xrest/xsp/model/strategy/AllByKey.java
@@ -3,6 +3,7 @@ package org.openntf.xrest.xsp.model.strategy;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.openntf.xrest.xsp.dsl.DSLBuilder;
 import org.openntf.xrest.xsp.exec.Context;
 import org.openntf.xrest.xsp.exec.DatabaseProvider;
 import org.openntf.xrest.xsp.exec.ExecutorException;
@@ -13,6 +14,7 @@ import org.openntf.xrest.xsp.model.RouteProcessor;
 
 import com.ibm.commons.util.io.json.JsonJavaArray;
 
+import groovy.lang.Closure;
 import lotus.domino.Database;
 import lotus.domino.Document;
 import lotus.domino.DocumentCollection;
@@ -21,15 +23,42 @@ import lotus.domino.View;
 
 public class AllByKey extends AbstractKeyViewDatabaseStrategy implements StrategyModel<DocumentListDataContainer, JsonJavaArray> {
 
+	private String modeValue;
+	private Closure<?> modeMatchCl;
+
+	public void mode(final String mode) {
+		this.modeValue = mode;
+	}
+
+	public void mode(final Closure<?> modeCl) {
+		this.modeMatchCl = modeCl;
+	}
+
+	public String getModeValue(final Context context) {
+		if (this.modeMatchCl != null) {
+			return (String) DSLBuilder.callClosure(modeMatchCl, context);
+		} else {
+			return modeValue;
+		}
+	}
+
 	@Override
 	public DocumentListDataContainer buildDataContainer(final Context context) throws ExecutorException {
 		try {
-			Database dbAccess = DatabaseProvider.INSTANCE.getDatabase(getDatabaseNameValue(context), context.getDatabase(), context.getSession());
+			Database dbAccess = DatabaseProvider.INSTANCE.getDatabase(getDatabaseNameValue(context), context.getDatabase(), context
+					.getSession());
 			View viewAccess = dbAccess.getView(getViewNameValue(context));
 			viewAccess.setAutoUpdate(false);
 			List<Document> docs = new ArrayList<Document>();
 			String varValue = context.getRouterVariables().get(getKeyVariableValue(context));
-			DocumentCollection dcl = viewAccess.getAllDocumentsByKey(varValue);
+
+			boolean exact = false;
+			String mode = getModeValue(context);
+			if (null != mode && mode.equalsIgnoreCase("exact")) {
+				exact = true;
+			}
+
+			DocumentCollection dcl = viewAccess.getAllDocumentsByKey(varValue, exact);
 
 			Document docNext = dcl.getFirstDocument();
 			while (docNext != null) {
@@ -45,7 +74,8 @@ public class AllByKey extends AbstractKeyViewDatabaseStrategy implements Strateg
 	}
 
 	@Override
-	public JsonJavaArray buildResponse(final Context context, final RouteProcessor routeProcessor, final DataContainer<?> dc) throws NotesException {
+	public JsonJavaArray buildResponse(final Context context, final RouteProcessor routeProcessor, final DataContainer<?> dc)
+			throws NotesException {
 		DocumentListDataContainer docListDC = (DocumentListDataContainer) dc;
 		DocumentList2JsonConverter d2jc = new DocumentList2JsonConverter(docListDC, routeProcessor, context);
 		return d2jc.buildJsonFromDocument();
--- server/org.openntf.xrest.xsp/src/org/openntf/xrest/xsp/model/strategy/AllByKeyPaged.java
+++ server/org.openntf.xrest.xsp/src/org/openntf/xrest/xsp/model/strategy/AllByKeyPaged.java
@@ -1,5 +1,6 @@
 package org.openntf.xrest.xsp.model.strategy;
 
+import org.openntf.xrest.xsp.dsl.DSLBuilder;
 import org.openntf.xrest.xsp.exec.Context;
 import org.openntf.xrest.xsp.exec.DatabaseProvider;
 import org.openntf.xrest.xsp.exec.ExecutorException;
@@ -10,6 +11,7 @@ import org.openntf.xrest.xsp.model.RouteProcessor;
 
 import com.ibm.commons.util.io.json.JsonObject;
 
+import groovy.lang.Closure;
 import lotus.domino.Database;
 import lotus.domino.DocumentCollection;
 import lotus.domino.NotesException;
@@ -17,6 +19,25 @@ import lotus.domino.View;
 
 public class AllByKeyPaged extends AbstractKeyViewDatabaseStrategy implements
 		StrategyModel<DocumentListPaginationDataContainer, JsonObject> {
+
+	private String modeValue;
+	private Closure<?> modeMatchCl;
+
+	public void mode(final String mode) {
+		this.modeValue = mode;
+	}
+
+	public void mode(final Closure<?> modeCl) {
+		this.modeMatchCl = modeCl;
+	}
+
+	public String getModeValue(final Context context) {
+		if (this.modeMatchCl != null) {
+			return (String) DSLBuilder.callClosure(modeMatchCl, context);
+		} else {
+			return modeValue;
+		}
+	}
 
 	@Override
 	public DocumentListPaginationDataContainer buildDataContainer(final Context context) throws ExecutorException {
@@ -27,8 +48,13 @@ public class AllByKeyPaged extends AbstractKeyViewDatabaseStrategy implements
 			viewAccess.setAutoUpdate(false);
 			String varValue = context.getRouterVariables().get(getKeyVariableValue(context));
 
-			// TODO: add parameter to set exact/partial matching
-			DocumentCollection dcl = viewAccess.getAllDocumentsByKey(varValue, false);
+			boolean exact = false;
+			String mode = getModeValue(context);
+			if (null != mode && mode.equalsIgnoreCase("exact")) {
+				exact = true;
+			}
+
+			DocumentCollection dcl = viewAccess.getAllDocumentsByKey(varValue, exact);
 			int total = dcl.getCount();
 			int start = getParamIntValue(context.getRequest().getParameter("start"), DEFAULT_START);
 			int count = getParamIntValue(context.getRequest().getParameter("count"), DEFAULT_COUNT);
