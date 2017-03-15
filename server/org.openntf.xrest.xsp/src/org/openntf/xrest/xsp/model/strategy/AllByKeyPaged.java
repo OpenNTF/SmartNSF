5216174159aabaa6c5f3941c603aad36e765016f
Martin Jinoch <jinoch@gmail.com>
2017-03-15 21:13:07 +0100

    refactor: extract superclass

--- server/org.openntf.xrest.xsp/src/org/openntf/xrest/xsp/model/strategy/AllByKeyPaged.java
+++ server/org.openntf.xrest.xsp/src/org/openntf/xrest/xsp/model/strategy/AllByKeyPaged.java
@@ -1,6 +1,5 @@
 package org.openntf.xrest.xsp.model.strategy;
 
-import org.openntf.xrest.xsp.dsl.DSLBuilder;
 import org.openntf.xrest.xsp.exec.Context;
 import org.openntf.xrest.xsp.exec.DatabaseProvider;
 import org.openntf.xrest.xsp.exec.ExecutorException;
@@ -11,33 +10,13 @@ import org.openntf.xrest.xsp.model.RouteProcessor;
 
 import com.ibm.commons.util.io.json.JsonObject;
 
-import groovy.lang.Closure;
 import lotus.domino.Database;
 import lotus.domino.DocumentCollection;
 import lotus.domino.NotesException;
 import lotus.domino.View;
 
-public class AllByKeyPaged extends AbstractKeyViewDatabaseStrategy implements
+public class AllByKeyPaged extends AbstractAllByKeyViewDatabaseStrategy implements
 		StrategyModel<DocumentListPaginationDataContainer, JsonObject> {
-
-	private String modeValue;
-	private Closure<?> modeMatchCl;
-
-	public void mode(final String mode) {
-		this.modeValue = mode;
-	}
-
-	public void mode(final Closure<?> modeCl) {
-		this.modeMatchCl = modeCl;
-	}
-
-	public String getModeValue(final Context context) {
-		if (this.modeMatchCl != null) {
-			return (String) DSLBuilder.callClosure(modeMatchCl, context);
-		} else {
-			return modeValue;
-		}
-	}
 
 	@Override
 	public DocumentListPaginationDataContainer buildDataContainer(final Context context) throws ExecutorException {
@@ -48,13 +27,7 @@ public class AllByKeyPaged extends AbstractKeyViewDatabaseStrategy implements
 			viewAccess.setAutoUpdate(false);
 			String varValue = context.getRouterVariables().get(getKeyVariableValue(context));
 
-			boolean exact = false;
-			String mode = getModeValue(context);
-			if (null != mode && mode.equalsIgnoreCase("exact")) {
-				exact = true;
-			}
-
-			DocumentCollection dcl = viewAccess.getAllDocumentsByKey(varValue, exact);
+			DocumentCollection dcl = viewAccess.getAllDocumentsByKey(varValue, isExact(context));
 			int total = dcl.getCount();
 			int start = getParamIntValue(context.getRequest().getParameter("start"), DEFAULT_START);
 			int count = getParamIntValue(context.getRequest().getParameter("count"), DEFAULT_COUNT);
