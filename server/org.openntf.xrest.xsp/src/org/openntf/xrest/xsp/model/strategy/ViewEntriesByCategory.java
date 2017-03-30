6b8c9370430ba46dd3f0ce38af3fd11a5d7b75c1
Martin Jinoch <jinoch@gmail.com>
2017-03-30 16:38:57 +0200

    Squashed commit of the following:
    commit 3e46b0d340dbf919b5209893d8927a202a0d4543
    Author: Martin Jinoch <jinoch@gmail.com>
    Date:   Thu Mar 30 16:37:04 2017 +0200
    
        publish added strategies
    
    commit fa10e07a3fa592602d5f55e5e96901b258876101
    Author: Martin Jinoch <jinoch@gmail.com>
    Date:   Thu Mar 30 16:36:45 2017 +0200
    
        implement VIEWENTRIES_BY_CATEGORY_PAGED strategy
    
    commit ff29556007582fdd6b94dcd08683fc58dda2a6b2
    Author: Martin Jinoch <jinoch@gmail.com>
    Date:   Thu Mar 30 16:36:30 2017 +0200
    
        implement VIEWENTRIES_BY_CATEGORY strategy

---
+++ server/org.openntf.xrest.xsp/src/org/openntf/xrest/xsp/model/strategy/ViewEntriesByCategory.java
@@ -0,0 +1,70 @@
+package org.openntf.xrest.xsp.model.strategy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openntf.xrest.xsp.exec.Context;
+import org.openntf.xrest.xsp.exec.DatabaseProvider;
+import org.openntf.xrest.xsp.exec.ExecutorException;
+import org.openntf.xrest.xsp.exec.convertor.ViewEntryList2JsonConverter;
+import org.openntf.xrest.xsp.exec.datacontainer.ViewEntryListDataContainer;
+import org.openntf.xrest.xsp.model.DataContainer;
+import org.openntf.xrest.xsp.model.RouteProcessor;
+
+import com.ibm.commons.util.io.json.JsonJavaArray;
+
+import lotus.domino.Database;
+import lotus.domino.NotesException;
+import lotus.domino.View;
+import lotus.domino.ViewEntry;
+import lotus.domino.ViewNavigator;
+
+public class ViewEntriesByCategory extends AbstractAllByKeyViewDatabaseStrategy implements
+		StrategyModel<ViewEntryListDataContainer, JsonJavaArray> {
+
+	private Database dbAccess;
+	private View viewAccess;
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public ViewEntryListDataContainer buildDataContainer(final Context context) throws ExecutorException {
+		try {
+			dbAccess = DatabaseProvider.INSTANCE.getDatabase(getDatabaseNameValue(context), context.getDatabase(), context.getSession());
+			viewAccess = dbAccess.getView(getViewNameValue(context));
+			viewAccess.setAutoUpdate(false);
+			List<List<Object>> entries = new ArrayList<List<Object>>();
+			String varValue = context.getRouterVariables().get(getKeyVariableValue(context));
+			ViewNavigator vnav = viewAccess.createViewNavFromCategory(varValue);
+			ViewEntry entCurrent = vnav.getFirst();
+			while (entCurrent != null && entCurrent.isValid() && !entCurrent.isCategory() && !entCurrent.isConflict()) {
+				List<Object> columnValues = new ArrayList<Object>();
+				columnValues.addAll(entCurrent.getColumnValues());
+				// add UNID to the list of values for this row
+				columnValues.add(entCurrent.getUniversalID());
+				entries.add(columnValues);
+				ViewEntry nextEntry = vnav.getNext();
+				// recycle!
+				entCurrent.recycle();
+				entCurrent = nextEntry;
+			}
+			vnav.recycle();
+			return new ViewEntryListDataContainer(entries, viewAccess, dbAccess);
+		} catch (Exception ex) {
+			throw new ExecutorException(500, ex, "", "getmodel");
+		}
+	}
+
+	@Override
+	public JsonJavaArray buildResponse(final Context context, final RouteProcessor routeProcessor, final DataContainer<?> dc)
+			throws NotesException {
+		// at this point we should already have dbAccess and viewAccess, but
+		// just to be on the safe side
+		if (null == dbAccess) {
+			dbAccess = DatabaseProvider.INSTANCE.getDatabase(getDatabaseNameValue(context), context.getDatabase(), context.getSession());
+			viewAccess = dbAccess.getView(getViewNameValue(context));
+		}
+		ViewEntryListDataContainer veldc = (ViewEntryListDataContainer) dc;
+		ViewEntryList2JsonConverter d2jc = new ViewEntryList2JsonConverter(veldc, routeProcessor, viewAccess, context);
+		return d2jc.buildJsonFromDocument();
+	}
+}
---
+++ server/org.openntf.xrest.xsp/src/org/openntf/xrest/xsp/model/strategy/ViewEntriesByCategoryPaged.java
@@ -0,0 +1,97 @@
+package org.openntf.xrest.xsp.model.strategy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openntf.xrest.xsp.exec.Context;
+import org.openntf.xrest.xsp.exec.DatabaseProvider;
+import org.openntf.xrest.xsp.exec.ExecutorException;
+import org.openntf.xrest.xsp.exec.convertor.ViewEntryListPaged2JsonConverter;
+import org.openntf.xrest.xsp.exec.datacontainer.ViewEntryListPaginationDataContainer;
+import org.openntf.xrest.xsp.model.DataContainer;
+import org.openntf.xrest.xsp.model.RouteProcessor;
+
+import com.ibm.commons.util.io.json.JsonObject;
+
+import lotus.domino.Database;
+import lotus.domino.NotesException;
+import lotus.domino.View;
+import lotus.domino.ViewEntry;
+import lotus.domino.ViewNavigator;
+
+public class ViewEntriesByCategoryPaged extends AbstractAllByKeyViewDatabaseStrategy implements
+		StrategyModel<ViewEntryListPaginationDataContainer, JsonObject> {
+
+	private Database dbAccess;
+	private View viewAccess;
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public ViewEntryListPaginationDataContainer buildDataContainer(final Context context) throws ExecutorException {
+		try {
+			dbAccess = DatabaseProvider.INSTANCE.getDatabase(getDatabaseNameValue(context), context.getDatabase(), context.getSession());
+			viewAccess = dbAccess.getView(getViewNameValue(context));
+			viewAccess.setAutoUpdate(false);
+
+			int start = getParamIntValue(context.getRequest().getParameter("start"), DEFAULT_START);
+			int count = getParamIntValue(context.getRequest().getParameter("count"), DEFAULT_COUNT);
+			String varValue = context.getRouterVariables().get(getKeyVariableValue(context));
+			List<List<Object>> entries = new ArrayList<List<Object>>();
+
+			ViewNavigator vnav = viewAccess.createViewNavFromCategory(varValue);
+			vnav.setCacheGuidance(count, ViewNavigator.VN_CACHEGUIDANCE_READSELECTIVE);
+			vnav.setEntryOptions(ViewNavigator.VN_ENTRYOPT_NOCOUNTDATA);
+
+			int total = -1;
+			String totals = context.getRequest().getParameter("totals");
+			// skip counting only if we have parameter totals=off
+			// if parameter is omitted, we assume it is on
+			totals = null == totals ? "on" : totals;
+			if (!totals.equals("off")) {
+				total = vnav.getCount();
+			}
+
+			int skippedEntries = 0;
+			// skip only when not starting at 1
+			if (start > 1) {
+				// skip counts from 0, so for start==2 we should skip(1)
+				skippedEntries = vnav.skip(start - 1);
+			}
+
+			if (skippedEntries == start - 1) {
+				int i = 0;
+				ViewEntry entCurrent = vnav.getCurrent();
+				while (entCurrent != null && entCurrent.isValid() && !entCurrent.isCategory() && !entCurrent.isConflict() && i < count) {
+					List<Object> columnValues = new ArrayList<Object>();
+					columnValues.addAll(entCurrent.getColumnValues());
+					// add UNID to the list of values for this row
+					columnValues.add(entCurrent.getUniversalID());
+					entries.add(columnValues);
+					i++;
+					ViewEntry nextEntry = vnav.getNext();
+					// recycle!
+					entCurrent.recycle();
+					entCurrent = nextEntry;
+				}
+			}
+			vnav.recycle();
+			return new ViewEntryListPaginationDataContainer(entries, start, total, viewAccess, dbAccess);
+		} catch (Exception ex) {
+			throw new ExecutorException(500, ex, "", "getmodel");
+		}
+	}
+
+	@Override
+	public JsonObject buildResponse(final Context context, final RouteProcessor routeProcessor, final DataContainer<?> dc)
+			throws NotesException {
+		// at this point we should already have dbAccess and viewAccess, but
+		// just to be on the safe side
+		if (null == dbAccess) {
+			dbAccess = DatabaseProvider.INSTANCE.getDatabase(getDatabaseNameValue(context), context.getDatabase(), context.getSession());
+			viewAccess = dbAccess.getView(getViewNameValue(context));
+		}
+		ViewEntryListPaginationDataContainer veldc = (ViewEntryListPaginationDataContainer) dc;
+		ViewEntryListPaged2JsonConverter d2jc = new ViewEntryListPaged2JsonConverter(veldc, routeProcessor, viewAccess, context);
+		return d2jc.buildJsonFromDocument();
+	}
+}
