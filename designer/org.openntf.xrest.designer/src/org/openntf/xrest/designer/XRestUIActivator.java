aa57740b91c547919851196d2cc834c309ec442d
Christian Guedemann <christian.guedemann@webgate.biz>
2017-05-22 11:21:05 +0200

    Type-A-Head for DSL

--- designer/org.openntf.xrest.designer/src/org/openntf/xrest/designer/XRestUIActivator.java
+++ designer/org.openntf.xrest.designer/src/org/openntf/xrest/designer/XRestUIActivator.java
@@ -4,7 +4,18 @@ import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.resource.ImageRegistry;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.openntf.xrest.designer.dsl.DSLRegistry;
+import org.openntf.xrest.designer.dsl.MapContainer;
+import org.openntf.xrest.designer.dsl.MethodContainer;
+import org.openntf.xrest.xsp.exec.Context;
+import org.openntf.xrest.xsp.model.EventType;
+import org.openntf.xrest.xsp.model.MapJsonType;
+import org.openntf.xrest.xsp.model.RouteProcessor;
+import org.openntf.xrest.xsp.model.Router;
+import org.openntf.xrest.xsp.model.Strategy;
 import org.osgi.framework.BundleContext;
+
+import groovy.transform.stc.MapEntryOrKeyValue;
 
 /**
  * The activator class controls the plug-in life cycle
@@ -17,6 +28,8 @@ public class XRestUIActivator extends AbstractUIPlugin {
 
 	// The shared instance
 	private static XRestUIActivator plugin;
+
+	private final DSLRegistry dslRegistry = new DSLRegistry("router", Router.class);
 
 	/**
 	 * The constructor
@@ -33,6 +46,7 @@ public class XRestUIActivator extends AbstractUIPlugin {
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
+		initRegistry();
 	}
 
 	/*
@@ -66,5 +80,30 @@ public class XRestUIActivator extends AbstractUIPlugin {
 			ImageDescriptor desc = imageDescriptorFromPlugin(PLUGIN_ID, "icons/" + icon);
 			registry.put(icon, desc);
 		}
+	}
+
+	public DSLRegistry getDSLRegistry() {
+		return dslRegistry;
+	}
+
+	private void initRegistry() {
+		dslRegistry.addClosureObjecForMethod("GET", RouteProcessor.class);
+		dslRegistry.addClosureObjecForMethod("PUT", RouteProcessor.class);
+		dslRegistry.addClosureObjecForMethod("POST", RouteProcessor.class);
+		dslRegistry.addClosureObjecForMethod("DELETE", RouteProcessor.class);
+		for (Strategy strat : Strategy.values()) {
+			MethodContainer mc = MethodContainer.buildContainerWithCondition(RouteProcessor.class, "strategy", strat.name(), strat.getModelClass());
+			dslRegistry.addClosureObjecForMethod(mc);
+		}
+		for (EventType event : EventType.values()) {
+			MapContainer mc = MapContainer.buildMakKeyWithClosure(RouteProcessor.class, "events", event.name(), Context.class, Object.class);
+			dslRegistry.addMapKeyClosure(mc);
+		}
+		dslRegistry.addMapKeyClosure(MapContainer.buildMapKeyWithValue(RouteProcessor.class, "mapJson", "json", String.class));
+		dslRegistry.addMapKeyClosure(MapContainer.buildMapKeyWithValue(RouteProcessor.class, "mapJson", "type", MapJsonType.class));
+		dslRegistry.addMapKeyClosure(MapContainer.buildMapKeyWithValue(RouteProcessor.class, "mapJson", "isformula", Boolean.class));
+		dslRegistry.addMapKeyClosure(MapContainer.buildMapKeyWithValue(RouteProcessor.class, "mapJson", "readonly", Boolean.class));
+		dslRegistry.addMapKeyClosure(MapContainer.buildMapKeyWithValue(RouteProcessor.class, "mapJson", "writeonly", Boolean.class));
+		dslRegistry.addMapKeyClosure(MapContainer.buildMapKeyWithValue(RouteProcessor.class, "mapJson", "formula", String.class));
 	}
 }
--- designer/org.openntf.xrest.designer/src/org/openntf/xrest/designer/dsl/DSLRegistry.java
+++ designer/org.openntf.xrest.designer/src/org/openntf/xrest/designer/dsl/DSLRegistry.java
@@ -1,13 +1,17 @@
 package org.openntf.xrest.designer.dsl;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class DSLRegistry {
 
 	private final Class<?> baseClass;
 	private final String baseAlias;
-	private final Map<String, Class<?>> registeredMethods = new HashMap<String, Class<?>>();
+	private final Map<String, MethodContainer> registeredMethods = new HashMap<String, MethodContainer>();
+	private final Map<String, List<MapContainer>> registeredMaps = new HashMap<String, List<MapContainer>>();
 
 	public DSLRegistry(String alias, Class<?> class1) {
 		this.baseAlias = alias;
@@ -15,11 +19,12 @@ public class DSLRegistry {
 	}
 
 	public void addClosureObjecForMethod(String method, Class<?> executorClass) {
-		addClosureObjecForMethod(baseClass, method, executorClass);
+		MethodContainer mc = MethodContainer.buildContainer(baseClass, method, executorClass);
+		addClosureObjecForMethod(mc);
 	}
 
-	public void addClosureObjecForMethod(Class<?> cl, String method, Class<?> executorClass) {
-		registeredMethods.put(cl.getName() + "#" + method, executorClass);
+	public void addClosureObjecForMethod(MethodContainer mc) {
+		registeredMethods.put(mc.getContainerClass().getName() + "#" + mc.getMethodName(), mc);
 	}
 
 	public Object getObjectForClosureInMethod(String method) {
@@ -27,23 +32,75 @@ public class DSLRegistry {
 	}
 
 	public Class<?> getObjectForClosureInMethod(Class<?> cl, String method) {
-		return registeredMethods.get(cl.getName() + "#" + method);
+		return registeredMethods.get(cl.getName() + "#" + method).getClosureClass();
 	}
 
 	public void addClosureObjectForMethodWithCondtion(String method, Object condition, Class<?> executorClass) {
-		addClosureObjecForMethodWithCondition(baseClass, method, condition, executorClass);
+		MethodContainer mc = MethodContainer.buildContainerWithCondition(baseClass, method, condition.toString(), executorClass);
+		addClosureObjecForMethodWithCondition(mc);
 	}
 
-	public void addClosureObjecForMethodWithCondition(Class<?> cl, String method, Object condition, Class<?> executorClass) {
-		registeredMethods.put(cl.getName() + "#" + method + "!" + condition.toString(), executorClass);
+	public void addClosureObjecForMethodWithCondition(MethodContainer mc) {
+		registeredMethods.put(mc.getContainerClass().getName() + "#" + mc.getMethodName() + "!" + mc.getCondition(), mc);
 	}
 
 	public Class<?> getObjectForClosureInMethodByCondition(Class<?> cl, String method, Object condition) {
-		return registeredMethods.get(cl.getName() + "#" + method + "!" + condition.toString());
+		return registeredMethods.get(cl.getName() + "#" + method + "!" + condition.toString()).getClosureClass();
 	}
 
 	public Class<?> getObjectForClosureInMethodByCondition(String method, Object condition) {
 		return getObjectForClosureInMethodByCondition(baseClass, method, condition);
 	}
-	
+
+	public boolean isMethodConditioned(String method) {
+		return isMethodConditioned(baseClass, method);
+	}
+
+	public boolean isMethodConditioned(Class<?> cl, String method) {
+		String toSearchFor = cl.getName() + "#" + method + "!";
+		for (String key : registeredMethods.keySet()) {
+			if (key.startsWith(toSearchFor)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public void addMapKeyClosure(MapContainer mc) {
+		String key = buildKeyForMapContainer(mc.getContainerClass(), mc.getMethodName());
+		List<MapContainer> mcs = null;
+		if (registeredMaps.containsKey(key)) {
+			mcs = registeredMaps.get(key);
+		} else {
+			mcs = new ArrayList<MapContainer>();
+			registeredMaps.put(key, mcs);
+		}
+		mcs.add(mc);
+	}
+
+	public List<String> getMapKeys(Class<?> cl, String method) {
+		List<String> keys = new ArrayList<String>();
+		String mcKey = buildKeyForMapContainer(cl, method);
+		if (registeredMaps.containsKey(mcKey)) {
+			List<MapContainer> mcs = registeredMaps.get(mcKey);
+			for (MapContainer mc : mcs) {
+				keys.add(mc.getKey());
+			}
+		}
+		return keys;
+	}
+
+	public List<MapContainer> getMapContainers(Class<?> cl, String method) {
+		String mcKey = buildKeyForMapContainer(cl, method);
+		if (registeredMaps.containsKey(mcKey)) {
+			return new ArrayList<MapContainer>(registeredMaps.get(mcKey));
+		}
+		return Collections.emptyList();
+		
+	}
+
+	private String buildKeyForMapContainer(Class<?> cl, String method) {
+		return cl.getName() + "#" + method;
+	}
+
 }
